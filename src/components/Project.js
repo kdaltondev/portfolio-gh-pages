aa41a3d9db1fd5627b3bf4077dfbeaf36492618c
dalton_k <longlivebicycles@gmail.com>
2023-07-14 09:13:09 -0400

    Added functionality to show correct thumbnail

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,7 @@
 import React from "react";
 import sitConmigoScreenshot from "./../img/sitConmigoThumbnail400.png";
+import newGloucesterScreenshot from "./../img/NewGloucesterThumbnail400.png";
+import stickyNoteScreenshot from "./../img/StickyNoteThumbnail400.png";
 
 export default function Project(props) {
   console.log(props)
@@ -8,7 +10,9 @@ export default function Project(props) {
       <div className="project-container">
       <h3><a href={props.data.link}>{props.data.title}</a></h3>
       <div className="project-thumbnail-container">
-        <img src={sitConmigoScreenshot} />
+        <img src={props.data.image==="sitConmigoScreenshot" ? sitConmigoScreenshot 
+        : props.data.image==="newGloucesterScreenshot" ? newGloucesterScreenshot
+      : stickyNoteScreenshot} />
         </div>
       <p>{props.data.description}</p>
       <h3>Skills</h3>
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -12,15 +12,15 @@ const projectData = [
       description:"App that allows users to create, edit and delete sticky notes.",
       link: "https://codesandbox.io/s/sticky-notes-4hfw79?file=/src/App.js",
       skills: ["ES6","JSX","Managing Dataflow","Manipulating the DOM"],
-      image: "sticky-note-screenshot.png",
+      image: "stickyNoteScreenshot",
       tools: ["CodeSandbox"]
     },
     {
       title: "First Congregational-Christian Church",
       description:"Re-design of a Wordpress site with dynamic news, events, and sermons featured on the homepage.  Back-end optimized for ease of use.",
-            link: "https://www.google.com",
+            link: "https://www.loom.com/share/cda0e1a23e6640a185f1a02ca36c81db?sid=16767c4c-8506-4e61-8258-3aa3669918d9",
       skills: ["Responsive Design"],
-      image: "",
+      image: "newGloucesterScreenshot",
       tools: [""]
     }
   ];
