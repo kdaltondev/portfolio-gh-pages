298978be08cc92eb6ed1d76e700477dc3b21af7e
dalton_k <longlivebicycles@gmail.com>
2023-07-14 14:20:02 -0400

    Added hamburger mobile menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function Navigation() {
+  const [mobileDisplay,setMobileDisplay]=useState(false)
+
+ const handleClick=()=>{
+    setMobileDisplay(!mobileDisplay)
+    console.log(mobileDisplay)
+  }
+  
   return (
+    
     <div className="navigation">
-      <nav className="navbar navbar-expand">
+
+      {/*Mobile Navigation Menu*/}
+
+
+      
+  
+  <button id="hamburger-menu" onClick={handleClick}>
+  {mobileDisplay && <div className="sidebar-navigation-menu"><nav>
+    <h3>Kate Dalton Portfolio</h3>
+    <ul>
+      <li onClick={handleClick}><a href="#about">About</a></li>
+      <li  onClick={handleClick}><a href="#skills">Skills</a></li>
+      <li onClick={handleClick}><a href="#projects">Projects</a></li>
+      <li onClick={handleClick}><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+  </div>}
+</button>
+<div class="overlay"></div>
+
+{/*Main Navigation*/}
+
+      <nav className="navbar navbar-expand" id="main-menu">
         <div className="container">
             <ul className="navbar-nav mr-auto">
               <li>Kate Dalton</li>
@@ -32,7 +62,7 @@ function Navigation() {
           
         </div>
       </nav>
-    </div>
+  </div>
   );
 }
 
