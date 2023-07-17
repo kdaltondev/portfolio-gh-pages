2ac2f19f7c851f30ddaef9ae7fe335b00cb871c2
dalton_k <longlivebicycles@gmail.com>
2023-07-17 16:13:13 -0400

    Changed Wordpress to WordPress

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -19,18 +19,18 @@ const projectData = [
     },
     {
       title: "First Congregational-Christian Church",
-      description:"Re-design of a Wordpress site with dynamic news, events, and sermons featured on the homepage.  Back-end optimized for ease of use using custom post types, Advanced Custom Fields, and a block child theme.",
+      description:"Re-design of a WordPress site with dynamic news, events, and sermons featured on the homepage.  Back-end optimized for ease of use using custom post types, Advanced Custom Fields, and a block child theme.",
             link: "https://www.loom.com/share/cda0e1a23e6640a185f1a02ca36c81db?sid=16767c4c-8506-4e61-8258-3aa3669918d9",
       skills: ["Responsive Design", "Block Theme", "theme.json","Custom Post Types"],
       image: "newGloucesterScreenshot",
       imageAlt: "picture of New Gloucester Congregational Church Steeple from above",
-      tools: ["Wordpress","Local WP","Advanced Custom Fields"]
+      tools: ["WordPress","Local WP","Advanced Custom Fields"]
     },
     {
     title: "Mobile Trivia",
     description: "Web App that pulls 5 multiple choice questions at a time from Open Trivia DB.  Allows user to choose answers and check them.  Once the answers have been checked, a play again button appears to allow the user to pull 5 new questions.",
           link: "https://kdaltondev.github.io/mobiletriviaunlimited/",
-    skills: ["Javascript","React","JSX","Bootstrap","API"],
+    skills: ["JavaScript","React","JSX","Bootstrap","APIs"],
     image: "mobileTriviaUnlimitedScreenshot",
     imageAlt: "Three trivia questions with multiple answers under each one.  Correct answers indicated by green and incorrect by red.",
     tools: ["VS Code", "Git", "GitHub"]
